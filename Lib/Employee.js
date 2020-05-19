7325663ff20843bd5bfef2e659f9fdb13c96532b
Michael Tirona <tironamichael@gmail.com>
2020-05-19 09:34:51 -0700

    Code unfinished, sorry :/

--- Lib/Employee.js
+++ Lib/Employee.js
@@ -1,5 +1,5 @@
 class Employee {
-    constructor(name, title, id, email) {
+    constructor(name, title, id) {
         this.name = name
         this.title = title
         this.id = 100
--- app.js
+++ app.js
@@ -7,21 +7,42 @@ const Intern = require('./Lib/Intern')
 
 const writeFileSync = promisify(writeFile)
 
-const top = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-`
+let team = []
 
-writeFileSync('index.html', top)
-    .then(() => {
-        prompt([
-            type: 'input',
-            name: ''
-        ])
-    })
+const manager = [
+    {
+        type: 'input',
+    name: 'name',
+    message: 'Enter Manager\'s name:'
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'Enter Manager\'s ID: '
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'Enter Office Number'
+    }
+]
+// const top = `
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//   <meta charset="UTF-8">
+//   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//   <title>Document</title>
+// </head>
+// <body>
+// `
+
+// writeFileSync('index.html', top)
+//     .then(() => {
+//         prompt([
+//             type: 'input',
+//             name: ''
+//         ])
+//     })
+
+console.log(manager)
