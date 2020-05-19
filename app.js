0df8872d84cb2d9d7eb3881875341fe53c3fa4f2
Michael Tirona <tironamichael@gmail.com>
2020-05-18 19:29:40 -0700

    Worked on app.js file

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 const { prompt } = require('inquirer')
+const { writeFile } = require('fs')
+const { promisify } = require('util')
 const Manager = require('./Lib/Manager')
 const Engineer = require('./Lib/Engineer')
 const Intern = require('./Lib/Intern')
 
+const writeFileSync = promisify(writeFile)
+
+const top = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+`
+
+writeFileSync('index.html', top)
+    .then(() => {
+        prompt([
+            type: 'input',
+            name: ''
+        ])
+    })
