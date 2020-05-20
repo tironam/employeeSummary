3b9a7c0a77d2335b16ac795bbd4dd801f7d05265
Michael Tirona <tironamichael@gmail.com>
2020-05-19 19:50:38 -0700

    Added code to write file

--- app.js
+++ app.js
@@ -9,6 +9,19 @@ const writeFileSync = promisify(writeFile)
 const readFileSync = promisify(readFile)
 const appendFileSync = promisify(appendFile)
 
+const top = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+            integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    </head>
+    <body>
+`
+
 let team = []
 
 const manager = () => {
@@ -43,8 +56,8 @@ const manager = () => {
         .catch(err => console.log(err))
 } 
 
-const writeManager = () => {
-    
+const writeManager = (answers) => {
+    appendFileSync('/output/index.html', )
 }
 
 // const top = `
