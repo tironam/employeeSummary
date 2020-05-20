62e7044eef4f38ba0397cd6d1e446b193d29c405
Michael Tirona <tironamichael@gmail.com>
2020-05-19 17:45:37 -0700

    Enigneer test passed

--- Lib/Engineer.js
+++ Lib/Engineer.js
@@ -3,7 +3,7 @@ const Employee = require('./Employee')
 class Engineer extends Employee {
     constructor (name, id, email, github) {
         super (name, id, email)
-        this.gitHub = github
+        this.github = github
         this.getGithub = () => {
             return github
         }
